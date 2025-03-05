c6f858185c8b548ab5c75cdf6e4cb7c6f81ad537
Angela Waithera <128139429+Angieswork@users.noreply.github.com>
2025-03-05 14:46:04 +0300

    Add files via upload

---
+++ MY keras.py
@@ -0,0 +1,31 @@
+# Import necessary libraries
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+# Load the MNIST dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Preprocess the data
+X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32') / 255
+X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32') / 255
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# Build the neural network model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=5, batch_size=64)
+
+# Evaluate the model
+model.evaluate(X_test, y_test)
